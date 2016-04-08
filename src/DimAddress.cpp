06d02b114742873bc185f81a870656cc803a968b
Sean Stephens <seanastephens@email.arizona.edu>
2016-04-08 01:35:19 -0700

    Drastically simplify DimAddress encoding.

--- src/DimAddress.cpp
+++ src/DimAddress.cpp
@@ -3,31 +3,56 @@
 #include <numeric>
 #include <iostream>
 #include <iterator>
+#include <cassert>
 
-DimAddress::DimAddress(std::string key) : key("1" + key) { }
+DimAddress::DimAddress(std::string key) : key(key) { }
+
+/*
+ * To support ANY aggregation on a 1-dim address over { 0, 1 } of depth k, we
+ * need to be able to uniquely encode any string over the alphabet { 0, 1, * }
+ * of length up to k, which means that we need 3^k possible values. We might
+ * want 25-level quadtrees, hence uint64_t.
+ *
+ * These contribute to the size of the keys in the MediatedCube
+ * address_to_prefix table (and there are a lot of keys!), so:
+ *
+ * - If we change to a more compact representation of an Address (sequence of
+ *   DimAddress), then it would make sense to change this to smaller types when
+ *   possible.
+ *
+ * - If you only want prefixes and not arbitrary addresses, you only need 32
+ *   bits (5 for how long the prefix is, 27 for the pattern itself).
+ *
+ * Now, how do we actually map { w in { 0, 1, * }, |w| <= k } to uint64_t?
+ * For simplicity, we limit the maximum address length lower than we
+ * have to and just concatentate the prefix with the prefix length.
+ */
+uint64_t compute_prefix(std::string k, uint32_t prefix_len) {
+	assert(prefix_len <= k.size());
+	assert(k.size() < 32);
+
+	std::string prefix = k.substr(0, prefix_len);
+
+	size_t len = 0;
+	uint64_t path = prefix_len > 0 ? stol(prefix, &len, 2) : 0;
+	assert(len == prefix.size());
+
+	return (path << 32) | prefix_len;
+}
 
 std::vector<uint64_t>
 DimAddress::prefixes() {
-	std::vector<uint64_t> prefixes(key.size());
-
-	size_t len;
-	uint64_t x = stol(key, &len, 2);
-
-	for(std::string::size_type i = 0; i < key.size(); i++) {
-		prefixes[i] = (((x >> i) & 0xFFFFFFFF) << 32 ) | (key.size() - i);
-	}
+	std::vector<uint64_t> prefixes;
+	for(size_t i = 0; i <= key.size(); i++)
+		prefixes.push_back(compute_prefix(key, i));
 
 	return prefixes;
 }
 
 uint64_t DimAddress::exact() {
-	uint64_t x = stol(key, 0, 2);
-	return (x << 32 ) | key.size();
+	return compute_prefix(key, key.size());
 }
 
 std::ostream &operator<<(std::ostream & o, DimAddress const& d) {
-	o << "DimAddress(";
-	std::copy(d.key.begin(), d.key.end(), std::ostream_iterator<char>(o, ","));
-	o << ")";
-	return o;
+	return o << "DimAddress(" << d.key << ")";
 }
--- test/DimAddressTest.cpp
+++ test/DimAddressTest.cpp
@@ -2,6 +2,10 @@
 
 #include "DimAddress.h"
 
+template <typename T>
+void CHECK_NOT_EQUAL(T a, T b) {
+	BOOST_CHECK_PREDICATE(std::not_equal_to<T>(), (a)(b));
+}
 
 BOOST_AUTO_TEST_SUITE(DimAddressTest)
 
@@ -10,6 +14,11 @@ BOOST_AUTO_TEST_CASE(FullPrefixTest)
 	DimAddress d("010");
 
 	BOOST_CHECK(4 == d.prefixes().size());
+}
+
+BOOST_AUTO_TEST_CASE(NoCollisionOnZeroPrefix)
+{
+	CHECK_NOT_EQUAL(DimAddress("011").exact(), DimAddress("010").exact());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
